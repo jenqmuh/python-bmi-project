16bf7c991b9ddfec65068c949fa8cbaa9991d80f
jenqmuh <hsujm.ncyu@gmail.com>
2026-09-12 16:05:29 +0800

    增加 BMI 分類

--- bmi.py
+++ bmi.py
@@ -1,2 +1,12 @@
 def calculate_bmi(weight, height) :
     return weight / (height ** 2)
+
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "體重過輕"
+    elif bmi < 24:
+        return "正常範圍"
+    elif bmi < 27:
+        return "過重"
+    else:
+        return "肥胖"
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from bmi import calculate_bmi
+from bmi import calculate_bmi, bmi_category
 
 weight = float(input("請輸入體重(kg)："))
 height = float(input("請輸入身高(m)："))
 
 bmi = calculate_bmi(weight, height)
+category = bmi_category(bmi)
 
 print(f"BMI = {bmi:.2f}")
+print(f"分類：{category}")
